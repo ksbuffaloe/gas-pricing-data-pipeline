4f8854b6e66c602930dab111b6ff6615ed47603a
ksbuffaloe <ksbuffaloe@gmail.com>
2025-08-02 22:12:46 -0600

    adding descriptive comments

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from ingest_api import run_ingest
 from run_transfrom import run_transform
 
+# This script runs the data pipeline by running both the ingestion and transformation steps.
 def run_pipeline():
+    # Start the data ingestion process 
     print("Starting data ingestion...")
     run_ingest()
     print("Data ingestion completed successfully!")
-
+    
+    # Start the data transformation process
     print("Starting data transformation...")
     run_transform()
     print("Data transformation completed successfully!")
