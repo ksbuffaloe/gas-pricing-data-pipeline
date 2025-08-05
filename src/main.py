a9e215aab8483907307c31e50da13f63a612af01
ksbuffaloe <ksbuffaloe@gmail.com>
2025-08-04 22:18:08 -0600

    adding a feaute to log when pipeline is run by task scheduler

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
 from ingest_api import run_ingest
 from run_transfrom import run_transform
+import logging
+import os
+from datetime import datetime
+
+# Get the directory where the script is located
+current_dir = os.path.dirname(os.path.abspath(__file__))
+log_file_path = os.path.join(current_dir, "pipeline.log")
+
+# Configure logging
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+logging.info("Pipeline run started")
 
 # This script runs the data pipeline by running both the ingestion and transformation steps.
 def run_pipeline():
